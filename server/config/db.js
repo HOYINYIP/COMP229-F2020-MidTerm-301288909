79bc925272dc9d2402d61f91d91273f7b664b701
HOYINYIP <jones.yipkgi@gmail.com>
2023-03-04 14:11:23 -0500

    First attempt

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localho301288st/books229"
+  "URI": "mongodb+srv://jonesyip:S8zIhaAKguOjJsCJ@midtermdb.d0hsyde.mongodb.net/?retryWrites=true&w=majority"
+
+};
